e6df773c939a799bbf0cf4aa70725eb3c5791ae5
berkayoruc <berkayoruc@hotmail.com>
2024-05-12 09:05:26 +0300

    first init

---
+++ assets/interaction-limit-57a352ce.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-3cfb730f.js";import{a,i as s,t as e}from"./index-43b56403.js";const r=(o,t)=>s(o)?e(o,t,"pk.eyJ1Ijoib3J1Y2JlIiwiYSI6ImNqbzFodzFoODBhc2UzcW9ha2FqNnVsdXkifQ.xpQYSb2Cvam_khG8VLdIWw"):{url:o};new a.Map({container:"mapContainer",style:"mapbox://styles/mapbox/streets-v12",zoom:10,center:[29,41.015137],transformRequest:r});
